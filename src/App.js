c9ed8ac75028ca34f9d15aa6f23288d6912c5c9e
Mdmosley12 <mdmosley12@gmail.com>
2023-05-24 16:50:52 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
